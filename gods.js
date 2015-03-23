3aa52f691d59655918873789e1cd2e91a2ccf86d
Brian Capouch <brianc@saintjoe.edu>
2015-03-23 01:58:49 -0400

    Examples taken from Jared's Friday homework

---
+++ gods.js
@@ -0,0 +1,11 @@
+var GODS = [
+  {name: "agni", favor: 1250, release: 1338436800000},
+  {name: "ullr", favor: 5500, release: 1395201600000},
+  {name: "athena", favor: 5500, release: 1370404800000},
+  {name: "thor", favor: 0, release: 1346990400000},
+  {name: "serqet", favor: 5500, release: 1405396800000},
+  {name: "mercury", favor: 5500, release: 1380686400000},
+  {name: "bellona", favor: 11000, release: 1424840400000},
+  {name: "nox", favor: 5500, release: 1414555200000}
+];
+
---
+++ newgods.js
@@ -0,0 +1,39 @@
+load('gods.js');
+
+// Map old millisecond GOD objects into new array with friendly Dates
+var friendlyGods = GODS.map(function(god) {
+  var newObj = {};
+
+  // This "sort of" blew up in my face . . and the reason why is interesting!!
+  // newObj = god;
+
+  // And this will too, if the object has function properties!!
+  //  for different reasons . . .
+  newObj = JSON.parse(JSON.stringify(god));
+  newObj.release =  new Date(newObj.release).toLocaleDateString('EN-US','%d-%b-%Y');
+  return newObj;
+  });
+
+// Show results of mapping transform
+//print('Original dataset:');
+//print(JSON.stringify(GODS) + '\n');
+//print('Dataset with dates mapped to human readable format:');
+//print(JSON.stringify(friendlyGods) + '\n');
+
+// Original line from Jared's code
+// var testDate = 1357016400000;
+// Convert into a formatted Date object
+var testDate = new Date(1357016400000).toLocaleDateString('EN-US','%d-%b-%Y');
+
+// Attempting find gods created within the last year
+print('Gods released after: ' + testDate);
+var selections = friendlyGods.filter(
+  function(element) {
+    if (new Date(element.release) > new Date(testDate)) 
+      return element; 
+});
+
+// Print results
+selections.forEach(function(element) {
+  print(element.name);
+  });
