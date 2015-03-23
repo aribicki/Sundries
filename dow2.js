ddf33793d979a74e6a87614906266a52b365910e
Brian Capouch <brianc@saintjoe.edu>
2015-03-23 02:11:16 -0400

    Clean up code, change hardwired values to user input

--- dow2.js
+++ dow2.js
@@ -2,6 +2,7 @@
 var ENGLISH = 0,
     SPANISH = 1;
 
+// Note that we revert to using TWO anonymous functions here
 var dayName = function() {
   var names = [];
   names[ENGLISH] = ["Sunday", "Monday", "Tuesday", "Wednesday",
@@ -14,4 +15,11 @@ var dayName = function() {
     };
 }();
 
-print(dayName(SPANISH, 1));
+write('Enter number 1 for English, 2 for Spanish: ');
+var lang = readline();
+write('Enter day number 1-7 for which day: ');
+var dayNo = readline();
+// Adjust for computer counting
+lang = (lang == 1)?0:1;
+dayNo = --dayNo;
+print(dayName(lang, dayNo));
